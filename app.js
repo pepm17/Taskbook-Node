5face2edc27c871c9184a8248095cc90ee8ba49f
Jorge Castro <mcand2297@gmail.com>
2019-01-11 06:05:10 -0500

    Peticiones entre servidores
    Este complemento le permite enviar y depurar solicitudes de dominios cruzados sin recibir errores de origen cruzado.

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+    });//permite que se hagan peticiones al servidor desde uno externo
 app.use('/taskbook', api);
 
 module.exports = app;
